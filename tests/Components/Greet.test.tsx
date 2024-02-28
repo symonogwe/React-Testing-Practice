70b040ddcbb9e7ee70d95dd7b61941224c0ab952
Symon Ogwe <symonogwe@gmail.com>
2024-02-28 23:58:14 +0300

    Test Greet component

---
+++ tests/Components/Greet.test.tsx
@@ -0,0 +1,21 @@
+import { it, expect, describe } from "vitest";
+import Greet from "../../src/components/Greet";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+
+describe("Greet", () => {
+  it("Should render heading when the name is not provided", () => {
+    render(<Greet name="Symon" />);
+    const heading = screen.getByRole("heading", { name: /symon/i });
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/symon/i);
+  });
+
+  it("should render login button when the name is not provided", () => {
+    render(<Greet />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+    screen.debug();
+  });
+});
