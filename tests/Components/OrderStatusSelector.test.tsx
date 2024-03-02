afa270d252417f92057858da366e9929b7a4d538
Symon Ogwe <symonogwe@gmail.com>
2024-03-02 16:28:57 +0300

    Refactor OrderStatusSelector tests

--- tests/Components/OrderStatusSelector.test.tsx
+++ tests/Components/OrderStatusSelector.test.tsx
@@ -4,30 +4,35 @@ import { Theme } from "@radix-ui/themes";
 import userEvent from "@testing-library/user-event";
 
 describe("OrderStatusSelector", () => {
-  it("Should render New as the initial text", () => {
+  const renderComponent = () => {
+    const onChange = vi.fn();
+
     render(
       <Theme>
-        <OrderStatusSelector onChange={vi.fn()} />
+        <OrderStatusSelector onChange={onChange} />
       </Theme>
     );
 
-    const button = screen.getByRole("combobox");
-    expect(button).toHaveTextContent(/new/i);
+    return {
+      trigger: screen.getByRole("combobox"),
+      getOptions: () => screen.findAllByRole("option"),
+    };
+  };
+
+  it("Should render New as the initial text", () => {
+    const { trigger } = renderComponent();
+
+    expect(trigger).toHaveTextContent(/new/i);
   });
 
   it("Should display other options when button is clicked", async () => {
     const user = userEvent.setup();
 
-    render(
-      <Theme>
-        <OrderStatusSelector onChange={vi.fn()} />
-      </Theme>
-    );
+    const { trigger, getOptions } = renderComponent();
 
-    const button = screen.getByRole("combobox");
-    await user.click(button);
+    await user.click(trigger);
 
-    const options = await screen.findAllByRole("option");
+    const options = await getOptions();
     expect(options.length).toEqual(3);
 
     const labels = options.map((option) => option.textContent);
