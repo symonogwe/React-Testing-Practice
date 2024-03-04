335d11caa55f313f4b06dd6b952910729fc976dd
Symon Ogwe <symonogwe@gmail.com>
2024-03-04 12:16:32 +0300

    Install faker.js

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import { faker } from "@faker-js/faker";
 
 describe("group", () => {
-  it("should", async () => {
-    const response = await fetch("/categories");
-    const data = await response.json();
-    console.log(data);
-    expect(data).toHaveLength(3);
+  it("should", () => {
+    console.log({
+      name: faker.commerce.productName(),
+      price: faker.commerce.price({ min: 1, max: 100 }),
+    });
   });
 });
